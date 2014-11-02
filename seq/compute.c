08a05704677dcf9f9937eb6048ada2b45995807e
Ilias <ilias@Okeanos.(none)>
2014-11-02 02:16:47 +0000

    comments and some error checking

--- seq/compute.c
+++ seq/compute.c
@@ -3,8 +3,23 @@
 #include <stdlib.h>
 #include <string.h> // memcpy
 #include <float.h> // DBL
+#include <errno.h>
+#include "fail.h"
 #include "compute.h"
 
+#define TRUE 1
+
+/*
+ * Function:  do_compute 
+ * --------------------
+ * computes the heat dissipation of a cylindrical 
+ * surface presented as a N x M matrix:   
+ *
+ *  p: The struct containing the parameters of the input
+ *  
+ *  r: The struct to store the interesting results
+ *       
+ */
 void do_compute(const struct parameters* p, struct results *r) {
 	double dirnmul = sqrt(2)/(sqrt(2) + 1);
 	double diagnmul = 1.0/(sqrt(2) + 1);
@@ -14,26 +29,31 @@ void do_compute(const struct parameters* p, struct results *r) {
 	// allocate two temperature matrices, copy the initial state into t_prev
 	double *t_prev, *t_next;
 	t_prev = malloc(sizeof(double) * p->M * p->N);
+	if (t_prev == 0) die("Out of memory");
 	t_next = malloc(sizeof(double) * p->M * p->N);
-	memcpy(t_prev, p->tinit, sizeof(double) * p->M * p->N);
+	if (t_next == 0) die("Out of memory");
+
+
+	memcpy(t_prev, p->tinit, sizeof(double) * p->M * p->N); //@Alyssa i'd like to discuss memory efficiency as posed to the forum
 
 	// iteration count
 	size_t iter = 0;
 	
+	// start timing
 	struct timeval tv_start, tv_curr, tv_diff;
-	gettimeofday(&tv_start, NULL);
+	int ret = gettimeofday(&tv_start, NULL);
+	if (ret == -1) die(strerror(errno));
 	
 	r->maxdiff = DBL_MAX; // something comfortably over the threshold
-	//while (r->maxdiff >= p->threshold && iter++ < p->maxiter) {
-	while (1) {
+
+	while (TRUE) { 
 		int done = 0;
 		int do_reduction = 0;
-		if (iter++ >= p->maxiter) done = 1;
-		do_reduction = done;
-		if (iter % p->period == 0) do_reduction = 1;
+		if (iter++ >= p->maxiter) done = 1; // do the final iteration and finish
+		do_reduction = done; // if we are done do a final reduction
+		if (iter % p->period == 0) do_reduction = 1; // do a midrun reduction 
 
 		if (do_reduction) {
-			// FIXME: update time
 			r->niter = iter;
 			r->tmin = DBL_MAX;
 			r->tmax = DBL_MIN;
@@ -44,8 +64,9 @@ void do_compute(const struct parameters* p, struct results *r) {
 
 		// N rows, M columns
 		double *dst = t_next;
+		// traverse rows
 		for (size_t y = 0; y < p->N; ++y) {
-			const double *cnd = &cond[p->M * y];
+			const double *cnd = &cond[p->M * y]; 
 
 			const double *row = &t_prev[p->M * y];
 			// boundary conditions (from the initial state)
@@ -55,33 +76,44 @@ void do_compute(const struct parameters* p, struct results *r) {
 			const double *rowdown = &t_prev[p->M * (y + 1)];
 			if (y == p->N-1)
 				rowdown = &p->tinit[p->M * (p->N-1)];
-
+			// traverse the row (columns)
 			for (size_t x = 0; x < p->M; ++x, dst++) {
-				// wraparound
+				// wraparound idiom
 				size_t prev = (x == 0 ? p->M-1 : x-1);
 				size_t next = (x == p->M-1 ? 0 : x+1);
 
-				double ourcnd = cnd[x];
-				double directcnd = (1.0 - ourcnd) * dirnmul;
-				double diagcnd = (1.0 - ourcnd) * diagnmul;
+				double ourcnd = cnd[x]; // the point's conductivity
+				double directcnd = (1.0 - ourcnd) * dirnmul; // direct neighbours weighted conductivity
+				double diagcnd = (1.0 - ourcnd) * diagnmul; // diagonal neighbours weighted conductivity
 
+				/* 
+				*  computation of the resulting temprature based on own conductivity 
+				*  plus average weighted of direct neighbours plus average weighted of
+				*  diagonal neighbours. If this comment made you tired here take a break:
+				*  http://imgs.xkcd.com/comics/ballmer_peak.png
+				*/
 				double result = row[x] * ourcnd;
 
 				double directsum = rowup[x] + rowdown[x] + row[prev] + row[next];
 				result += directcnd * directsum/4.0;
 				double diagsum = rowup[prev] + rowup[next] + rowdown[prev] + rowdown[next];
 				result += diagcnd * diagsum/4.0;
-
-				*dst = result;
+				/*
+				*  reflect the point temprature result on the t_next state;
+				*  this allows independent computations between the points
+				*/
+				*dst = result; 
 
 				if (do_reduction) {
-					// FIXME: don't do this if we don't need it
+					// update minimum temprature if needed
 					if (result < r->tmin)
 						r->tmin = result;
+					// update maximum temprature if needed
 					if (result > r->tmax)
 						r->tmax = result;
+					// update the sum (not yet average)
 					r->tavg += result;
-
+					// update the maximum temprature difference if needed
 					double diff = abs(row[x] - *dst);
 					if (diff > r->maxdiff)
 						r->maxdiff = diff;
@@ -89,12 +121,16 @@ void do_compute(const struct parameters* p, struct results *r) {
 			}
 		}
 		if (do_reduction) {
-			gettimeofday(&tv_curr, NULL);
+			// get current time difference for the result report
+			ret = gettimeofday(&tv_curr, NULL);
+			if (ret == -1) die(strerror(errno));
 			timersub(&tv_curr, &tv_start, &tv_diff);
 			r->time = tv_diff.tv_sec + (tv_diff.tv_usec / 1000000.0);
-
+			// compute average temprature for the result report 
 			r->tavg /= (p->N * p->M);
 			if (r->maxdiff < p->threshold) done = 1;
+
+			// report state for debugging reasons and create current state frame
 			if(p->printreports) {
 				begin_picture(iter, p->M, p->N, p->io_tmin, p->io_tmax);
 				double *tptr = t_prev;
@@ -108,12 +144,15 @@ void do_compute(const struct parameters* p, struct results *r) {
 			}
 		}
 
-		if (done) break;
-		// swap the buffers
-		double *t_temp;
-		t_temp = t_prev;
-		t_prev = t_next;
-		t_next = t_temp;
+		if (!done) {
+			// swap the buffers 
+			double *t_temp;
+			t_temp = t_prev;
+			t_prev = t_next;
+			t_next = t_temp;
+		}
+		else // done; reached maxiter or maxdiff is smaller than threshold
+			break;
 	}
 
 	free(t_prev);
