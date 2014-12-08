0be934e4917299464965cbb3e7470dfffdc7c935
Ilias <ilias@Okeanos.(none)>
2014-12-08 20:18:06 +0100

    report

---
+++ acc/report/reduction.c
@@ -0,0 +1,12 @@
+// iterate over non-constant rows
+#pragma omp parallel for \reduction(min: tmin) reduction(max: tmax) 
+ reduction(max: maxdiff) reduction(+: tavg)
+#pragma acc parallel loop gang independent reduction(min: tmin)
+ reduction(max: tmax) reduction(max: maxdiff) 
+ reduction(+: tavg) present(dst[0:w*h], src[0:w*h])
+for (size_t y = 1; y < h - 1; ++y) {
+	#pragma acc loop vectors independent
+	for (size_t x = 1; x < w - 1; ++x) {
+		/* reduction computation here */
+	}
+}
